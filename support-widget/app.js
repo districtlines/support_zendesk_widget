495da65d13e2c445ed960b503ea0b8953eaa6543
cstudioakt <cory@studioakt.com>
2016-11-14 15:13:40 -0500

    updated for universal integration

--- support-widget/app.js
+++ support-widget/app.js
@@ -10,8 +10,8 @@
 			var ticketRequester = ticket.requester();
 
 			var customer = {
-				'orderID' : ticket.customField('custom_field_36462'),
-				'email' : ticketRequester.email()
+				'custom_field_id' : ticket.customField('custom_field_' + this.setting('custom_field_id')),
+				'email' : ticketRequester.email(),
 			};
 
 			this.switchTo('iframe', customer);
